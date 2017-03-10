b354b3542a8fbfe117a26ce3038ef8de9a4035ad
Gunnþór Karl Rafnsson <gkr6@hi.is>
2017-03-10 20:16:16 +0000

    breyta frá HTML EJS í jade

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var path = require('path');
-// var favicon = require('serve-favicon');
+var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -14,8 +14,9 @@ var app = express();
 console.log("anyone?");
 
 app.set('views', path.join(__dirname, 'views'));
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
+// app.engine('html', require('ejs').renderFile);
+// app.set('view engine', 'html');
+app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express EJS UP IN THIS!' });
+  res.render('index', { title: 'Express wassup wassup wassup!' });
 });
 
 module.exports = router;
